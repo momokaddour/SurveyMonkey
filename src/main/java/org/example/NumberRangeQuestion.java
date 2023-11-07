3eca85012e81a3713aa350287e790da8b7d6e0cd
Kousha-Motazedian <k.d.Motazedian@Gmail.com>
2023-11-06 22:25:29 -0500

    trying this again

---
+++ src/main/java/org/example/NumberRangeQuestion.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class NumberRangeQuestion extends AbstractQuestion implements Question{
+    private String question;
+    private int minNumber;
+    private int maxNumber;
+
+    @Override
+    public void setQuestion(String q) {
+        question = q;
+    }
+
+    @Override
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setMaxNumber(int maxNumber) {
+        this.maxNumber = maxNumber;
+    }
+
+    public void setMinNumber(int minNumber) {
+        this.minNumber = minNumber;
+    }
+
+    public int getMaxNumber() {
+        return maxNumber;
+    }
+
+    public int getMinNumber() {
+        return minNumber;
+    }
+
+    @Override
+    public String toString() {
+        return question;
+    }
+}
