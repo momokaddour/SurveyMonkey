f26723668acd9461b8478db04b1d38ecf8df2b76
Kousha-Motazedian <k.d.Motazedian@Gmail.com>
2023-11-10 17:41:47 -0500

    adding javadocs to Question and Survey Classes

--- src/main/java/org/example/SurveyManager.java
+++ src/main/java/org/example/SurveyManager.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * SurveyManager Class, manages all the surveys and can compile their results
+ * @author Kousha Motazedian
+ */
 @Entity
 public class SurveyManager {
     @OneToMany(cascade = CascadeType.ALL)
@@ -15,11 +19,18 @@ public class SurveyManager {
     @GeneratedValue
     private Integer id;
 
+    /**
+     * Constructor for the survey manager
+     */
     public SurveyManager() {
         surveys = new ArrayList<>();
     };
 
 
+    /**
+     * Compiles the results of a survey based on its ID
+     * @param id, Integer
+     */
     public void compile(Integer id) {
         Survey s = getSurvey(id);
 
@@ -27,14 +38,29 @@ public class SurveyManager {
         //aggregate.add(a);
     }
 
+    /**
+     * Answering a specific survey
+     * @param id Integer
+     * @param f Form
+     */
     public void answerSurvey(Integer id, Form f) {
         Survey s = getSurvey(id);
         //s.addForm(f); add when form classes are added to src
     }
+
+    /**
+     * Add a survey to be managed by the manager
+     * @param s Survey
+     */
     public void addSurvey(Survey s) {
         surveys.add(s);
     }
 
+    /**
+     * Get a survey based on its ID
+     * @param id, Integer
+     * @return Survey
+     */
     public Survey getSurvey(Integer id) {
         for(Survey s : surveys){
             if(Objects.equals(s.getSurveyID(), id)){
@@ -44,14 +70,26 @@ public class SurveyManager {
         return null;
     }
 
+    /**
+     * Returns a list of all surveys managed
+     * @return List<Surveys>
+     */
     public List<Survey> getSurveys() {
         return surveys;
     }
 
+    /**
+     * Set the ID of the Survey Manager
+     * @param id Integer
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Get the ID of the Survey Manager
+     * @return Integer
+     */
     public Integer getId() {
         return id;
     }
